ab7fde55dedbf87d9c71ede7e9ea48993dc7af22
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-04-07 21:55:40 +0300

    feat: адаптив готов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,15 @@ import { useState, useEffect, memo } from 'react'
 import Chips from '../Chips/Chips'
 import CountrySlider from '../CountrySlider/CountrySlider'
 import Button from '../Button/Button'
-import TextInput from '../TextInput/TextInput'
+import Search from '../Search/Search'
 import './App.scss'
 
 function App() {
-  const [searchType, setSearchType] = useState('beach');
-  const [searchValue, setSearchValue] = useState('');
-  const [currentCountry, setCurrentCountry] = useState('');
-
-  // const initialPlaceholder = 'Поиск по стране, региону, городу';
+  const [searchType, setSearchType] = useState('beach')
+  const [searchValue, setSearchValue] = useState('')
+  const [currentCountry, setCurrentCountry] = useState('')
   const [mainButtonText, setMainButtonText] = useState('')
-  
+
   useEffect(() => {
     if (searchType === 'beach') setMainButtonText('Все пляжи страны')
     else if (searchType === 'breezzorPass') setMainButtonText('Все отели страны')
@@ -28,10 +26,10 @@ function App() {
     <div className="app">
       <form className="app__form" name="breezzor" onSubmit={handleSubmit}>
         <CountrySlider currentCountry={currentCountry} setCurrentCountry={setCurrentCountry} />
-        <Chips searchType={searchType} setSearchType={setSearchType}/>
-        <TextInput searchType={searchType} searchValue={searchValue} setSearchValue={setSearchValue}/>
-        <div className='app__buttons-overlay'>
-          <Button >На карте</Button>
+        <Chips searchType={searchType} setSearchType={setSearchType} />
+        <Search searchType={searchType} searchValue={searchValue} setSearchValue={setSearchValue} />
+        <div className="app__buttons-overlay">
+          <Button>На карте</Button>
           <Button main={true}>{mainButtonText}</Button>
         </div>
       </form>
@@ -40,4 +38,4 @@ function App() {
   )
 }
 
-export default memo(App);
+export default memo(App)
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,28 +1,28 @@
 import { useState, useEffect, memo } from 'react'
 import { InputIcon } from '../../images/images'
-import './TextInput.scss'
+import './Search.scss'
 
-const TextInput = ({ searchType, searchValue, setSearchValue }) => {
-  const initialPlaceholder = 'Поиск по стране, региону, городу';
+const Search = ({ searchType, searchValue, setSearchValue }) => {
+  const initialPlaceholder = 'Поиск по стране, региону, городу'
   const [placeholder, setPlaceholder] = useState(initialPlaceholder)
-  
+
   useEffect(() => {
     if (searchType === 'beach') setPlaceholder(initialPlaceholder + ', название пляжа')
     else if (searchType === 'breezzorPass') setPlaceholder(initialPlaceholder + ', названию отеля')
-    else if (searchType === 'impressions') setPlaceholder(initialPlaceholder + ', названию впечатления')
+    else if (searchType === 'impressions')
+      setPlaceholder(initialPlaceholder + ', названию впечатления')
     else if (searchType === 'routes') setPlaceholder(initialPlaceholder + ', названию маршрута')
   }, [searchType])
-  
+
   const handleChangeValue = (e) => {
-    setSearchValue(e.target.value);
+    setSearchValue(e.target.value)
   }
 
   return (
-    <section className='search'>
-      <input 
+    <section className="search">
+      <input
         className="search__input"
         type="text"
-        name="text"
         placeholder={placeholder}
         onChange={handleChangeValue}
         value={searchValue}
@@ -32,4 +32,4 @@ const TextInput = ({ searchType, searchValue, setSearchValue }) => {
   )
 }
 
-export default memo(TextInput);
+export default memo(Search)
